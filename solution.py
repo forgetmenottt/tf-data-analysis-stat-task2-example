2953c8bb21342a938b8ca2771c856561be6d89be
forgetmenottt <129880361+forgetmenottt@users.noreply.github.com>
2023-04-05 16:42:26 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,7 +12,7 @@ def solution(p: float, x: np.array) -> tuple:
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
     varian = np.var(x)
-    hiqv1 = st.chi2.ppf(1-alpha/2, 1)
-    hiqv2 = st.chi2.isf(1-alpha/2, 1)
-    return ((varian / hiqv1) ** (0.5)) / 6  , \
-           ((varian / hiqv2) ** (0.5)) / 6
+    hiqv1 = st.chi2.ppf(1-alpha/2, x.size-1)
+    hiqv2 = st.chi2.isf(1-alpha/2, x.size-1)
+    return (((x.size-1) * varian / hiqv1) ** (0.5)) / 6, \
+           (((x.size-1) * varian / hiqv2) ** (0.5)) / 6
