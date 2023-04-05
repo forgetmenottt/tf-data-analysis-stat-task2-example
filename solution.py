5dd53769a3809581e57d1a28b787f945385d7d6c
forgetmenottt <129880361+forgetmenottt@users.noreply.github.com>
2023-04-05 17:51:29 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import scipy.stats as st
-
+import statistics
 
 
 chat_id = 1372197133 # Ваш chat ID, не меняйте название переменной
@@ -11,7 +11,7 @@ def solution(p: float, x: np.array) -> tuple:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    varian = np.var(x)
+    varian = statistics.variance(x)
     hiqv1 = st.chi2.ppf(1-alpha/2, x.size-1)
     hiqv2 = st.chi2.isf(1-alpha/2, x.size-1)
     return (((x.size-1) * varian / hiqv1) ** (0.5)) / 6, \
